d2fc628573165add54dfe4d429111f4c5d31f16f
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-11-24 23:10:51 +0530

    issue fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import { Navbar } from './components/Navbar';
+import logo from "./logo.svg";
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-  
-     <Navbar/>
+      <Navbar />
+      <Home />
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,14 @@
 export const Home = () =>{
 
     return <>
-    
+    <div className="flex px-36 py-24 items-center gap-28 self-stretch border border-red-400 font-[Inter] text-left">
+
+        <div>
+            <p className="border border-red-400 font-[Inter] font-medium text-6xl text-[#4D4D4D] self-stretch  ">Lessons and insights <br /><span className="text-[#4CAF4F]">from 8 years</span> </p> 
+            <p className="text-[#717171]">Where to grow your business as a photographer: site or social media?</p>
+        </div>
+        <div></div>
+    </div>
     
     </>
 }
