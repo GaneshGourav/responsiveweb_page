b499f2a3244decdddc11ce4a4160eaf15d890112
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-11-24 20:38:48 +0530

    navbar created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,29 @@
-// import logo from "../assests/Logo (1).svg"
+import logo from "../assests/Logo (1).svg"
 
-// export const Navbar = () =>{
+export const Navbar = () =>{
 
     
-//     return <>
+    return <>
     
     
-//     <div className="flex border=1px solid red border border-sky-500 justify-around h-10 align-baseline items-center">
-// <div><img src={logo} alt="logo" /></div>
-// <div className="flex justify-around border border-red-300 w-1/2">
-//     <p>Home</p>
-//     <p>Service</p>
-//     <p>Feature</p>
-//     <p>Product</p>
-//     <p>Testimonial</p>
-//     <p>FAQ</p>
-// </div>
-// <div className="flex justify-between border border-red-200 w-32">
-//     <p>Login</p>
-//     <p>Sign up</p>
-// </div>
+    <div className="flex border=1px solid red border border-sky-500 justify-around h-10 align-baseline items-center">
+<div><img src={logo} alt="logo" /></div>
+<div className="flex justify-around border border-red-300 w-1/2">
+    <p>Home</p>
+    <p>Service</p>
+    <p>Feature</p>
+    <p>Product</p>
+    <p>Testimonial</p>
+    <p>FAQ</p>
+</div>
+<div className="flex justify-between border border-red-200 w-32">
+    <p>Login</p>
+    <p>Sign up</p>
+</div>
 
 
-//     </div>
+    </div>
     
     
-//     </>
-// }
+    </>
+}
