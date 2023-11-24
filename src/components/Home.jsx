4fe7606f26dff9dae303f3fef187ab8dc46f3511
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-11-24 23:28:47 +0530

    hero-left design-done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,14 +3,16 @@ export const Home = () => {
     <>
       <div className="flex px-36 py-24 items-center gap-28 self-stretch border border-red-400 font-[Inter] text-left">
         <div>
-          <p className="border border-red-400 font-[Inter] font-medium text-6xl text-[#4D4D4D] self-stretch  ">
+          <p className="font-[Inter] font-medium text-6xl text-[#4D4D4D] self-stretch  ">
             Lessons and insights <br />
             <span className="text-[#4CAF4F]">from 8 years</span>{" "}
           </p>
-          <p className="text-[#717171] border border-red-400 text-base">
+          <p className="text-[#717171]  text-base mt-4">
             Where to grow your business as a photographer: site or social media?
           </p>
-          <button className="rounded bg-[#4CAF4F] px-8 py-3 text-white">Register</button>
+          <button className="rounded bg-[#4CAF4F] px-8 py-3 text-white mt-8">
+            Register
+          </button>
         </div>
         <div></div>
       </div>
